75852e1c4b0ef9e87a43b463bc7242ef8de52e52
Gilmar Pereira de Alcântara <gilmardealcantara@gmail.com>
2017-01-27 10:01:02 -0200

    Add tests to graphs titles.

---
+++ tests/application_tests/test_graphs_titles.py
@@ -0,0 +1,15 @@
+from flask import g
+from dataviva.apps.title.views import get_graphs_title
+from test_base import BaseTestCase
+
+
+class GraphsTitlesTests(BaseTestCase):
+
+    def setUp(self):
+        g.locale = 'pt'
+
+    def test_titles_graphs_not_is_null(self):
+        assert len(get_graphs_title()) > 0;
+
+    def test_titles_graphs_is_a_string(self):
+        assert type(get_graphs_title()) == str;
